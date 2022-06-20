3ee1a8228b20c2bc551d7f84abb949f397c45ca3
Oleg Zagorevsky <pboul-ooo@yandex.ru>
2022-06-20 17:35:51 +0300

    feat: user

--- src/store/reducers/userReduser.ts
+++ src/store/reducers/userReduser.ts
@@ -1,13 +1,4 @@
-interface UserState {
-  users: any[];
-  loading: boolean;
-  error: null | string;
-}
-
-interface UserAction {
-  type: string;
-  payload?: any;
-}
+import { UserAction, UserActionTypes, UserState } from "../../types/user";
 
 const initialState = {
   loading: false,
@@ -17,11 +8,11 @@ const initialState = {
 
 export const useReducer = (state = initialState, action:UserAction): UserState => {
   switch (action.type) {
-    case "FETCH_USERS":
+    case UserActionTypes.FETCH_USERS:
       return { loading: true, users: [], error: null };
-    case "FETCH_USERS_SUCCESS":
+    case UserActionTypes.FETCH_USERS_SUCCESS:
       return { loading: false, users: action.payload, error: null };
-    case "FETCH_USERS_ERROR":
+    case UserActionTypes.FETCH_USERS_ERROR:
       return { loading: false, users: [], error: null };
     default:
       return state
